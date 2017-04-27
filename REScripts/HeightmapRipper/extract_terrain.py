47b7253a278e11e22de050a17844b65670dac2af
jos <joseph_delgado@colpal.com>
2017-04-26 20:26:01 -0400

    Don't fork a bunch of shit xd

--- REScripts/HeightmapRipper/extract_terrain.py
+++ REScripts/HeightmapRipper/extract_terrain.py
@@ -9,20 +9,20 @@ outputDir = sys.argv[2]
 fileNames = filter(lambda v: 'hght' in v, os.listdir(terrainDir))
 for fileName in fileNames:
     print(fileName)
-    os.system("mono ../../BoTK/bin/Debug/BoTK.exe --input '" + terrainDir + "/" + fileName + "' --output " + outputDir + " &")
+    os.system("mono ../../BoTK/bin/Debug/BoTK.exe --input '" + terrainDir + "/" + fileName + "' --output " + outputDir)
 
 fileNames = filter(lambda v: 'mate' in v, os.listdir(terrainDir))
 for fileName in fileNames:
     print(fileName)
-    os.system("mono ../../BoTK/bin/Debug/BoTK.exe --input '" + terrainDir + "/" + fileName + "' --output " + outputDir + " &")
+    os.system("mono ../../BoTK/bin/Debug/BoTK.exe --input '" + terrainDir + "/" + fileName + "' --output " + outputDir)
 
 fileNames = filter(lambda v: 'water' in v, os.listdir(terrainDir))
 for fileName in fileNames:
     print(fileName)
-    os.system("mono ../../BoTK/bin/Debug/BoTK.exe --input '" + terrainDir + "/" + fileName + "' --output " + outputDir + " &")
+    os.system("mono ../../BoTK/bin/Debug/BoTK.exe --input '" + terrainDir + "/" + fileName + "' --output " + outputDir)
 
 fileNames = filter(lambda v: 'grass' in v, os.listdir(terrainDir))
 for fileName in fileNames:
     print(fileName)
-    os.system("mono ../../BoTK/bin/Debug/BoTK.exe --input '" + terrainDir + "/" + fileName + "' --output " + outputDir + " &")
+    os.system("mono ../../BoTK/bin/Debug/BoTK.exe --input '" + terrainDir + "/" + fileName + "' --output " + outputDir)
 
